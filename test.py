003da6ea69a01f0a46fb07c2486d0aa131ec4fc6
dagross <glentrotter@gmail.com>
2022-05-23 20:15:46 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,89 @@
+from operator import truediv
+from re import X
+from unicodedata import name
+
+
+# hello="Hello"
+# World="World"
+# helloworld = hello +" "+ World
+# print (hello,World)
+
+# mylist=[]
+# mylist.append(1)
+# mylist.append(2)
+# mylist.append(3)
+# for x in mylist:
+#     print(x)
+
+# Lots=("Hello"+" ")*10
+# print(Lots)
+
+# ev_num=[2,4,6,8,10]
+# odd_num=[1,3,5,7,9]
+# texttest=["test1","test2"]
+# print(ev_num+odd_num+texttest)
+# test1="Jol111"
+# age=999999915
+# print ("Hello %s you are %.2f" % (test1 , age))
+
+# data=("Robby","Scuba", 14)
+# Textam="Hello %s %s, you are %f"
+# print (Textam % data)
+# print (Textam[5:8])
+
+# ab=1
+# Something=True
+# if Something != True:
+#     ab=ab+1
+#     print (ab)
+# else:
+#     print ("No")
+
+# # change this code
+# number = 16
+# second_number = 10
+# first_array = [1]
+# second_array = [1,2,3]
+
+# if number > 15:
+#     print("1")
+
+# if first_array:
+#     print("2")
+
+# if len(second_array) == 3:
+#     print("3")
+
+# if len(first_array) + len(second_array) == 4:
+#     print("4")
+
+# if first_array and first_array[0] == 1:
+#     print("5")
+
+# if not second_number:
+#     print("6")
+
+# for x in range (155):
+#     print (x)
+
+# ttx = 50
+#     for ttx > 0:
+#         print (ttx)
+#         ttx=ttx-1
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1  # This is the same as count = count + 1
+# x=50
+# print(x)
+# while x>0:
+#    # print (x)
+#     print ("Your number is %d" % x)
+#     x-=1
+
+for x in range (0,50):
+    str="*"*x
+    print (str)
+    
+
